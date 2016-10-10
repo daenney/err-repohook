7c44890566112d1e92580032dc49f36c4bbbc54f
Pavel Savchenko <asfaltboy@gmail.com>
2016-10-10 18:57:24 +0200

    Refactor: use explicit keyword arguments to render
    - locals() is pretty ugly

--- providers.py
+++ providers.py
@@ -28,6 +28,14 @@ class CommonGitWebProvider(object):
             message = self.msg_generic(body, repo, event_type)
         return message
 
+    def render_template(self, template='generic', **kwargs):
+        kwargs['repo_name'] = kwargs.get('repo_name') or self.name
+        return tenv().get_template('{0}.html'.format(template)).render(**kwargs)
+
+    def msg_generic(self, body, repo, event_type):
+        return self.render_template(
+            template='generic', body=body, repo=repo, event_type=event_type)
+
 
 class GithubHandlers(CommonGitWebProvider):
     name = 'Github'
@@ -35,51 +43,54 @@ class GithubHandlers(CommonGitWebProvider):
     def get_repo(self, body):
         return body['repository']['full_name']
 
-    def msg_generic(self, body, repo, event_type):
-        return tenv().get_template('generic.html').render(locals().copy())
-
     def msg_issues(self, body, repo):
-        action = body['action']
-        number = body['issue']['number']
-        title = body['issue']['title']
-        user = body['issue']['user']['login']
-        url = body['issue']['url']
-        is_assigned = body['issue']['assignee']
-        if is_assigned is not None:
-            assignee = body['issue']['assignee']['login']
-
-        return tenv().get_template('issues.html').render(locals().copy())
+        return self.render_template(
+            template='issues', body=body, repo=repo,
+            action=body['action'],
+            number=body['issue']['number'],
+            title=body['issue']['title'],
+            user=body['issue']['user']['login'],
+            url=body['issue']['url'],
+            is_assigned=body['issue']['assignee'],
+            assignee=body['issue']['assignee']['login'] if is_assigned else None
+        )
 
     def msg_pull_request(self, body, repo):
         action = body['action']
-        number = body['pull_request']['number']
         user = body['pull_request']['user']['login']
-        url = body['pull_request']['html_url']
-        merged = body['pull_request']['merged']
         if action == 'closed' and merged:
             user = body['pull_request']['merged_by']['login']
             action = 'merged'
         if action == 'synchronize':
             action = 'updated'
-        return tenv().get_template('pull_request.html').render(locals().copy())
+
+        return self.render_template(
+            template='pull_request', body=body, repo=repo,
+            action=action, user=user,
+            number=body['pull_request']['number'],
+            url=body['pull_request']['html_url'],
+            merged=body['pull_request']['merged'],
+        )
 
     def msg_pull_request_review_comment(self, body, repo):
-        action = body['action']
-        user = body['comment']['user']['login']
-        line = body['comment']['position']
-        l_url = body['comment']['html_url']
-        pr = body['pull_request']['number']
-        url = body['pull_request']['html_url']
-        if action == 'created':
-            action = 'commented'
-        return tenv().get_template('pull_request_review_comment.html').render(locals().copy())
+        return self.render_template(
+            template='pull_request_review_comment', body=body, repo=repo,
+            action='commented' if body['action'] == 'created' else body['action'],
+            user=body['comment']['user']['login'],
+            line=body['comment']['position'],
+            l_url=body['comment']['html_url'],
+            pr=body['pull_request']['number'],
+            url=body['pull_request']['html_url'],
+        )
 
     def msg_push(self, body, repo):
-        user = body['pusher']['name']
-        commits = len(body['commits'])
-        branch = body['ref'].split('/')[-1]
-        url = body['compare']
-        return tenv().get_template('push.html').render(locals().copy())
+        return self.render_template(
+            template='push', body=body, repo=repo,
+            user=body['pusher']['name'],
+            commits=len(body['commits']),
+            branch=body['ref'].split('/')[-1],
+            url=body['compare'],
+        )
 
     def msg_status(*args):
         """Status events are crazy and free form. There's no sane, consistent
@@ -87,21 +98,23 @@ class GithubHandlers(CommonGitWebProvider):
         return None
 
     def msg_issue_comment(self, body, repo):
-        action = body['action']
-        user = body['comment']['user']['login']
-        number = body['issue']['number']
-        title = body['issue']['title']
-        url = body['issue']['html_url']
-        if action == 'created':
-            action = 'commented'
-        return tenv().get_template('issue_comment.html').render(locals().copy())
+        return self.render_template(
+            template='issue_comment', body=body, repo=repo,
+            action='commented' if body['action'] == 'created' else body['action'],
+            user=body['comment']['user']['login'],
+            number=body['issue']['number'],
+            title=body['issue']['title'],
+            url=body['issue']['html_url'],
+        )
 
     def msg_commit_comment(self, body, repo):
-        user = body['comment']['user']['login']
-        url = body['comment']['html_url']
-        line = body['comment']['line']
-        sha = body['comment']['commit_id']
-        return tenv().get_template('commit_comment.html').render(locals().copy())
+        return self.render_template(
+            template='commit_comment', body=body, repo=repo,
+            user=body['comment']['user']['login'],
+            url=body['comment']['html_url'],
+            line=body['comment']['line'],
+            sha=body['comment']['commit_id'],
+        )
 
 
 class GitLabHandlers(CommonGitWebProvider):
@@ -121,19 +134,20 @@ class GitLabHandlers(CommonGitWebProvider):
         mapped_event_type = self.map_event_type(event_type)
         return super(GitLabHandlers, self).create_message(body, mapped_event_type, repo)
 
-    def msg_generic(self, body, repo, event_type):
-        return tenv().get_template('generic.html').render(locals().copy())
-
     def msg_push(self, body, repo):
-        user = body['user_name']
-        commits = len(body['commits'])
-        branch = '/'.join(body['ref'].split('/')[2:])
-        url = body['commits'][-1]['url'] if commits else body['project']['web_url']
-        return tenv().get_template('push.html').render(locals().copy())
+        return self.render_template(
+            template='push', body=body, repo=repo,
+            user=body['user_name'],
+            commits=len(body['commits']),
+            branch='/'.join(body['ref'].split('/')[2:]),
+            url=body['commits'][-1]['url'] if commits else body['project']['web_url'],
+        )
 
     def msg_commit_comment(self, body, repo):
-        user = body['user']['name']
-        url = body['object_attributes']['url']
-        line = body['object_attributes']['note']
-        sha = body['object_attributes']['commit_id']
-        return tenv().get_template('commit_comment.html').render(locals().copy())
+        return self.render_template(
+            template='commit_comment', body=body, repo=repo,
+            user=body['user']['name'],
+            url=body['object_attributes']['url'],
+            line=body['object_attributes']['note'],
+            sha=body['object_attributes']['commit_id'],
+        )
