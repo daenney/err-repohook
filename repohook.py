993b235c575c65e7333407f8d1471a0a3339be20
louiz’ <louiz+github@louiz.org>
2017-08-12 13:47:13 +0200

    Update to be compatible with Err 5.x.x
    * Remove the deprecated message_type argument of the send function
    * Update the min_err_version 5.0.0

--- repohook.py
+++ repohook.py
@@ -28,7 +28,7 @@ README = 'https://github.com/daenney/err-repohook/blob/master/README.rst'
 
 class RepoHook(BotPlugin):
 
-    min_err_version = '2.1.0'
+    min_err_version = '5.0.0'
 
     def __init__(self, *args, **kwargs):
         super(RepoHook, self).__init__(*args, **kwargs)
@@ -408,7 +408,7 @@ class RepoHook(BotPlugin):
                     identifier = self.build_identifier(room)
                     if hasattr(self, 'join_room'):
                         self.join_room(identifier, username=config.CHATROOM_FN)
-                    self.send(identifier, message, message_type='groupchat')
+                    self.send(identifier, message)
         response.status = 204
         return None
 
